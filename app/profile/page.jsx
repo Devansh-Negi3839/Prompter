7667bf23502dbffc56830a365bc8ccf13d1f0622
Devansh Negi <devansh3839@gmail.com>
2024-03-30 13:07:44 +0530

    Fixed Suspense everywhere

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
@@ -48,13 +48,15 @@ const MyProfile = () => {
   };
 
   return (
-    <Profile
-      name='My'
-      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
-      data={myPosts}
-      handleEdit={handleEdit}
-      handleDelete={handleDelete}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Profile
+        name='My'
+        desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+        data={myPosts}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
+    </Suspense>
   );
 };
 
